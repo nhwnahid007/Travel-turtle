9988a60d3b2c628a667efc78e235c62325424316
nhwnahid007 <nhwnahid@gmail.com>
2024-01-17 11:58:58 +0600

    Added navbar responsive

---
+++ navbar.js
@@ -0,0 +1,8 @@
+const headerTop = document.querySelector('.fa-bars');
+const navBars = document.querySelector('.navbars');
+
+headerTop.addEventListener('click', ()=>{
+    navBars.classList.toggle("active");
+
+}
+)
